c22989f2aaae0c934c67d90b5b8dc3bee34f815d
winstonjose01 <winston.jose@gmail.com>
2024-12-09 18:50:09 -0800

    revision-1

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 # include <deque>
 # include <vector>
 # include <queue>
+# include <ctime>
 using namespace std;
 
 
@@ -11,3 +12,28 @@ struct CoffeeNode {
     string drinkOrder;
     CoffeeNode* next;
 };
+
+struct Customer {
+    string customerName;
+    string order;
+};
+
+string getRandomName() {
+    string names[] = {"Alice", "John", "Bill", "Dianne", "Evan", "Lily"};
+    int rand_num = rand() % 5;
+    return names[rand_num];
+}
+
+string getRandomOrder(){
+    if ()
+}
+
+int main(){
+    srand(time(0));
+
+    CoffeeNode * head = nullptr;
+     
+     // Coffee Booth
+
+    return 0;
+}
