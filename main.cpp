f5b49b1b01162a5446fdbd627ab85a9b408b65d3
winstonjose01 <winston.jose@gmail.com>
2024-12-09 18:59:23 -0800

    revision-2

--- main.cpp
+++ main.cpp
@@ -19,13 +19,32 @@ struct Customer {
 };
 
 string getRandomName() {
-    string names[] = {"Alice", "John", "Bill", "Dianne", "Evan", "Lily"};
+    string names[] = {"Alice", "John", "Bill", "Dianne", "Evan"};
     int rand_num = rand() % 5;
     return names[rand_num];
 }
 
-string getRandomOrder(){
-    if ()
+string getRandomOrder(string &type){
+    int rand_num;
+    if (type == "coffee"){
+        string drinks[] = {"Latte","Cappucino","Expresso","Mocha","Americano"};
+        rand_num = rand() % 5;
+        return drinks[rand_num];
+    }
+    else if (type == "muffin"){
+        string muffins[] = {"Blueberry","Chocolate Chip","Banana Nut","Lemon","Apple"};
+        rand_num = rand() % 5;
+        return muffins[rand_num];
+    }
+    else if (type == "bracelet"){
+        string bracelets[] = {"Beaded","Leather","Gold","Silver","Woven"};
+        rand_num = rand() % 5;
+        return bracelets[rand_num];
+    }
+    else
+        string gadgets[] = {"P","Leather","Gold","Silver","Woven"};
+        rand_num = rand() % 5;
+        return bracelets[rand_num];
 }
 
 int main(){
@@ -33,7 +52,11 @@ int main(){
 
     CoffeeNode * head = nullptr;
      
-     // Coffee Booth
+     // Coffee Booth - Linked List
+     for (int i = 0; i < 3; ++i){
+        queueCoffee(head, getRandomName(); getRandomOrder("coffee");)
+     }
+
 
     return 0;
 }
