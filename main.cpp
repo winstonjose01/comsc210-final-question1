c15c11eee8a6ace508655c692cb6b709741edaac
winstonjose01 <winston.jose@gmail.com>
2024-12-09 19:19:32 -0800

    revision-3

--- main.cpp
+++ main.cpp
@@ -41,10 +41,38 @@ string getRandomOrder(string &type){
         rand_num = rand() % 5;
         return bracelets[rand_num];
     }
-    else
-        string gadgets[] = {"P","Leather","Gold","Silver","Woven"};
+    else{
+        string gadgets[] = {"Phone Charger","USB Drive","Speakers","Headphones","Power Banks"};
         rand_num = rand() % 5;
-        return bracelets[rand_num];
+        return gadgets[rand_num];
+    }
+
+}
+
+void queueCoffee (CoffeeNode* &head, const string& name, const string&order){
+    CoffeeNode* newNode = new CoffeeNode{name, order, nullptr};
+    if (!head) {
+        head = newNode;
+    } else {
+        CoffeeNode* temp = head;
+        while (temp->next) temp = temp->next;
+        temp->next = newNode;
+    }
+}
+
+void dequeueCoffee(CoffeeNode*& head) {
+    if (head) {
+        CoffeeNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+void displayCoffeeQueue(CoffeeNode* head) {
+    while (head) {
+        cout << head->customerName << " (" << head->drinkOrder << ") ";
+        head = head->next;
+    }
 }
 
 int main(){
@@ -54,9 +82,39 @@ int main(){
      
      // Coffee Booth - Linked List
      for (int i = 0; i < 3; ++i){
-        queueCoffee(head, getRandomName(); getRandomOrder("coffee");)
+        queueCoffee(head, getRandomName(), getRandomOrder("coffee"));
      }
 
+    deque<Customer> muffinQueue;
+    for (int i = 0; i < 3; ++i) {
+        muffinQueue.push_back({getRandomName(), getRandomOrder("muffin")});
+    }
+
+    vector<Customer> braceletQueue;
+    for (int i = 0; i < 3; ++i) {
+        braceletQueue.push_back({getRandomName(), getRandomOrder("bracelet")});
+    }
+
+    deque<Customer> gadgetQueue;
+    for (int i = 0; i < 3; ++i) {
+        gadgetQueue.push_back({getRandomName(), getRandomOrder("gadget")});
+    }
+
+    for (int round = 1; round <= 10; ++round){
+        cout << "\n=== Round " << round << " ===\n";
+
+        // Coffee Booth
+    cout << "Coffee Booth: ";
+        if (head) {
+            std::cout << "Serving " << head->customerName << " (" << head->drinkOrder << ")\n";
+            dequeueCoffee(head);
+        } else {
+            std::cout << "No customers to serve\n";
+        }
+
+        if (rand() %2 == 0) queueCoffee(head, getRandomName(),getRandomOrder("coffee"))
+
+    }
 
     return 0;
 }
